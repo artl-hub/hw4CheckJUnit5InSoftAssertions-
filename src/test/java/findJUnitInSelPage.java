bfda70fc409ccad5a16fbb664bf54404ef6cc7db
artl <33ttpp@gmail.com>
2023-11-20 22:45:58 +0100

    add worked code to git

---
+++ src/test/java/findJUnitInSelPage.java
@@ -0,0 +1,54 @@
+import com.codeborne.selenide.Configuration;
+
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Condition.*;
+import static com.codeborne.selenide.Selectors.byTagAndText;
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
+
+public class findJUnitInSelPage {
+
+
+    @BeforeAll
+    static void beforAll() {
+        Configuration.baseUrl = "https://github.com";
+        Configuration.browserSize = "1920x1080";
+        Configuration.pageLoadStrategy = "eager";
+        Configuration.holdBrowserOpen = true;
+        Configuration.timeout = 5000;
+    }
+
+    @Test
+        //ARRANGE
+    void fillFormTest() {
+        open("/selenide/selenide");
+
+        //ACT
+       $("#wiki-tab").click();
+
+       //ASSERT
+        $(byTagAndText("a","Soft assertions")).shouldBe(visible);
+
+        //ACT
+        $(byTagAndText("a","Soft assertions")).click();
+
+        //ASSERT
+        $("#user-content-3-using-junit5-extend-test-class")
+                .closest("h4").sibling(0).shouldHave(text("""
+                        @ExtendWith({SoftAssertsExtension.class})
+                        class Tests {
+                         @Test
+                        void test() {
+                        Configuration.assertionMode = SOFT;
+                        open("page.html");
+
+                        $("#first").should(visible).click();
+                        $("#second").should(visible).click();
+                        }
+                        }"""));
+    }
+}
+
+
